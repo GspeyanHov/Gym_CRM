6400b2118d117b855128c9782369e90aec6b05c4
gspeyanhov <gspeyanhov@gmail.com>
2024-05-24 19:24:30 +0400

    created facade

--- src/main/java/org/epam/gymCrm/Main.java
+++ src/main/java/org/epam/gymCrm/Main.java
@@ -1,14 +1,21 @@
 package org.epam.gymCrm;
 
-import javax.annotation.security.RunAs;
 import lombok.val;
 import org.epam.gymCrm.config.AppConfig;
+import org.epam.gymCrm.facade.GymCrmFacade;
+import org.epam.gymCrm.model.TrainingType;
 import org.epam.gymCrm.storage.InMemoryStorage;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         val context = new AnnotationConfigApplicationContext(AppConfig.class);
+        val facade = context.getBean(GymCrmFacade.class);
+        System.out.println(facade.getTraineeById(1));
+        System.out.println(facade.getAllTrainees());
+        System.out.println(facade.getTrainerById(2));
+        System.out.println(facade.getTrainingByType(TrainingType.AEROBICS));
+
         InMemoryStorage storage = context.getBean(InMemoryStorage.class);
         storage.getTraineeMap().forEach((k, v) -> System.out.println(k + ": " + v));
         storage.getTrainerMap().forEach((k, v) -> System.out.println(k + ": " + v));
